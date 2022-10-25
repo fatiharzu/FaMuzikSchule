b5ac8e5bde7522d3eafdc0bd2feb143b561814b3
arzu <fatiharzu1@gmail.com>
2022-10-25 13:44:41 +0200

    Initial commit

---
+++ src/de/farzu/main/Main.java
@@ -0,0 +1,11 @@
+package de.farzu.main;
+
+import de.farzu.ui.UiController;
+
+public class Main {
+    public static void main(String[] args) {
+        UiController uiController = new UiController();
+
+    }
+
+}
---
+++ src/de/farzu/ui/UiController.java
@@ -0,0 +1,30 @@
+package de.farzu.ui;
+
+public class UiController {
+//    region Konstanten
+//    endregion
+
+//    region Attribute
+//    endregion
+
+//    regionKonstruktor
+//    endregion
+
+//    regionMethoden
+    public void startUi(){
+        printApplicationName();
+        System.out.println("""
+                 __________________________
+                  ======
+                  |    | Note Musik Schule
+                 o'   o'
+                 __________________________
+                 """);
+    }
+
+    private void printApplicationName(){
+
+    }
+//    endregion
+
+}
